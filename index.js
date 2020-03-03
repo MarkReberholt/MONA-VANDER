7a4794291e1859ff3ef62c6d5759203f7f24f5c7
emmanielsen <emma6007@edu.ucl.dk>
2020-03-03 22:32:30 +0100

    prøver at lave en underline på tekst med en ease effekt

--- index.js
+++ index.js
@@ -11,9 +11,11 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
+
+
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
+  var slides = document.getElementsByClassName("mySlides"); 
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
