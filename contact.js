0e3b5ba1ee7b8cddacbac68987f0e0119361c315
idabille <60378712+idabille@users.noreply.github.com>
2020-03-06 13:52:46 +0100

    contact java script er tilføjet

---
+++ contact.js
@@ -0,0 +1,12 @@
+// JavaScript Document<script>
+	function yScrollHandler (){
+		var win=document.getElementById("peekaboo");
+		if((window.pageYOffset+window.innerHeight)>=document.body.offsetHeight){
+			win.style.transition = "right 0.7s ease-in-out 0s";
+			win.style.transition= "0px";
+	} else{
+		win.style.transition = "right 0.7s ease-in-out 0s";
+		win.style.right="-452px";
+	}
+}
+window.onscroll= yScrollHandler;
