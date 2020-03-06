df2dcdbc83018d7c36d0f7381c0d4adcba028090
Frederik-Harms <60352300+Frederik-Harms@users.noreply.github.com>
2020-03-06 14:05:36 +0100

    js på kontakt

--- contact.js
+++ contact.js
@@ -1,10 +1,11 @@
 // JavaScript Document<script>
-	function yScrollHandler (){
-		var win=document.getElementById("peekaboo");
-		if((window.pageYOffset+window.innerHeight)>=document.body.offsetHeight){
-			win.style.transition = "right 0.7s ease-in-out 0s";
-			win.style.transition= "0px";
-	} else{
+function yScrollHandler (){
+	var win = document.getElementsByClassName("peekaboo");
+	if((window.pageYOffset + window.innerHeight) >= document.body.offsetHeight){
+		win.style.transition = "right 0.7s ease-in-out 0s";
+		win.style.transition= "0px";
+	} 
+	else{
 		win.style.transition = "right 0.7s ease-in-out 0s";
 		win.style.right="-452px";
 	}
